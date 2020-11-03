bfca51fb0bbead1f60fd26de15076804c96f7d79
vlad00k <71580047+vlad00k@users.noreply.github.com>
2020-11-03 22:54:33 +0700

    Update 4.c

--- 4.c
+++ 4.c
@@ -1,56 +1,75 @@
+
 #include <stdio.h>
-#include <malloc.h>
-
-int get_determinant(int n, int matrix[][n]);
-void print_matrix(int n, int matrix[][n]);
-
 int main()
 {
-    int N;
-    printf("Enter N: ");
-    scanf("%d", &N);
-    int matrix[N][N];
-    printf("Enter matrix value with spaces:\n");
-    for (int i = 0; i < N; i++)
-        for (int k = 0; k < N; k++)
-            scanf("%d", &matrix[i][k]);
-
-    printf("Matrix determinant = %d", get_determinant(N, matrix));
-}
-
-int get_determinant(int n, int matrix[][n])
-{
-    // print_matrix(n, matrix);
-    // printf("\n")
-    int determinant = 0;
-    if (n == 2)
-        determinant = matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
-    else
+    int n;
+    scanf("%d",&n);
+    int matrix[n][n];
+    for(int i=0;i<n;i++)
     {
-        int sign = 1;
-        int submatrix[n - 1][n - 1];
-        for (int i = 0; i < n; i++)
+        for(int j=0;j<n;j++)
         {
-            for (int ii = 0; ii < i; ii++)
-                for (int kk = 0; kk < n - 1; kk++)
-                    submatrix[ii][kk] = matrix[ii][kk + 1];
-
-            for (int ii = i + 1; ii < n; ii++)
-                for (int kk = 1; kk < n; kk++)
-                    submatrix[ii - 1][kk - 1] = matrix[ii][kk];
-            determinant += sign * matrix[i][0] * get_determinant(n - 1, submatrix);
-            sign = -1 * sign;
+         scanf("%d",&matrix[i][j]);
         }
     }
-    return determinant;
-}
+printf("det=%d",det_calculate(n,matrix));
 
-void print_matrix(int n, int matrix[][n])
+
+}
+void print_matrix ( int n,int matrix[][n])
 {
-    for (int i = 0; i < n; i++)
+    for(int i=0;i<n;i++)
     {
-        for (int k = 0; k < n; k++)
-            printf("%d ", matrix[i][k]);
+        for(int j=0;j<n;j++)
+        {
+         printf("%d ",matrix[i][j]);
+        }
         printf("\n");
     }
+}
+
+int det_calculate(int n, int matrix[][n])
+{
+    int det=0;
+    if(n==2)
+    {
+        det=matrix[0][0]*matrix[1][1]-matrix[0][1]*matrix[1][0];
+        return det;
+    }
+    else if (n==1)
+        return matrix[0][0];
+    int sub_matrix[n-1][n-1];
+    for(int i=0;i<n;i++)
+    {
+     int a=matrix[0][i];
+     int sign;
+     if(i%2==0)
+     {
+        sign=1;
+     }
+     else
+     {
+        sign=-1;
+     }
+     a=sign*a;
+
+         for(int k=0;k<n-1;k++)
+        {
+            for(int j=0;j<i;j++)
+            {
+             sub_matrix[k][j]=matrix[k+1][j];
+            }
+        }
+
+        for(int k=0;k<n-1;k++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+             sub_matrix[k][j-1]=matrix[k+1][j];
+            }
+        }
+    det+=a*det_calculate(n-1,sub_matrix);
+    }
+    return det;
+
 }
